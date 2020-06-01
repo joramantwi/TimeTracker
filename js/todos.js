d73fb6703d0510b40345c350a62257ecbae31cc4
ngomri <57151912+ngomri@users.noreply.github.com>
2020-06-01 12:59:41 +0100

    Add files via upload

---
+++ js/todos.js
@@ -0,0 +1,26 @@
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "#trash", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span id='trash'><i class='fa fa-trash'></i></span><span id='clock'><i class='fas fa-clock'></span></i><span id='calendar'><i class='far fa-calendar-times'></span></i> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
